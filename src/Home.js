e452e04928330b5a09a5a8dc848c22934f865b66
Brandon Chase <brandonchase93@gmail.com>
2023-03-06 15:00:21 -0700

    events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('hello, ninjas');
+    }
+    
+
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e.target);
+    }
+    
     return ( 
         <div className="home">
             <h2>Home</h2>
+            <button onClick={handleClick}>Click Me</button>
+            {/* <button onClick={handleClickAgain('Mario')}>Click me again</button> */}
+            <button onClick={(e) => handleClickAgain('Mario', e)}>Click me again</button>
         </div>
      );
 }
