b1ee3824f202ddac4455e7fa4b70bd6d6d0bebb9
Brandon Chase <brandonchase93@gmail.com>
2023-03-06 13:45:27 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Navbar from './Navbar';
+import Home from './Home'
+
+function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  const link = 'http://www.google.com';
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />        
+      </div>
+    </div>
+  );
+}
+
+export default App;
